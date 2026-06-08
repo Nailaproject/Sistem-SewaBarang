7ffc8846a08432d708c5c413cc249cdd2da38ca5
Nailasapitri <sapitricrazi244@gmail.com>
2026-06-08 14:28:54 +0700

    Delete Data warehouse/sql directory

--- Data warehouse/sql/OLAP.sql
+++
@@ -1,9 +0,0 @@
-SELECT
-    k.nama_kategori,
-    COUNT(f.peminjaman_sk) AS total_transaksi,
-    SUM(f.total_biaya) AS total_pendapatan
-FROM fact_peminjaman_star f
-JOIN dim_kategori_barang k
-ON f.kategori_id = k.kategori_id
-GROUP BY k.nama_kategori
-ORDER BY total_pendapatan DESC;
